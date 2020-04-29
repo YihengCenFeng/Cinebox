4a7d22d91f7ef6930015ee097157b90f0e2db9ce
Yiheng Cen Feng <33232501+YihengCenFeng@users.noreply.github.com>
2020-04-29 12:29:53 -0400

    Initial Commit

---
+++ Cinebox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinebox
+//
+//  Created by Yiheng Cen Feng on 4/29/20.
+//  Copyright © 2020 Yiheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
